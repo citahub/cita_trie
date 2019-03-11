5b50690d1b5db8c6db0bfcb85d31ea6df131f270
laizy <aochyi@126.com>
2019-03-11 13:35:29 +0800

    cargo fmt

--- benches/insert_benchmark.rs
+++ benches/insert_benchmark.rs
@@ -1,20 +1,22 @@
-use criterion::{criterion_main, criterion_group, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion};
 
-use cita_trie::trie::{Trie, PatriciaTrie};
-use cita_trie::db::MemoryDB;
 use cita_trie::codec::RLPNodeCodec;
+use cita_trie::db::MemoryDB;
+use cita_trie::trie::{PatriciaTrie, Trie};
 
 fn insert_worse_case_benchmark(c: &mut Criterion) {
-    c.bench_function("insert 100 items", |b| b.iter(|| {
-        let mut memdb = MemoryDB::new();
-        let mut trie = PatriciaTrie::new(&mut memdb, RLPNodeCodec::default());
-        const N: usize = 100;
-        let mut buf = Vec::new();
-        for i in 0..N {
-            buf.push(i as u8);
-            trie.insert(&buf, b"testvalue").unwrap();
-        }
-    }));
+    c.bench_function("insert 100 items", |b| {
+        b.iter(|| {
+            let mut memdb = MemoryDB::new();
+            let mut trie = PatriciaTrie::new(&mut memdb, RLPNodeCodec::default());
+            const N: usize = 100;
+            let mut buf = Vec::new();
+            for i in 0..N {
+                buf.push(i as u8);
+                trie.insert(&buf, b"testvalue").unwrap();
+            }
+        })
+    });
 }
 
 criterion_group!(benches, insert_worse_case_benchmark);
